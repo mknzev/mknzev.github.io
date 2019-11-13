ad492c4e854bcf0add21db867be4638d86fba32d
mknzev <maksknyazev@gmail.com>
2019-11-13 16:30:23 +0300

    update zen_artile.js

--- yvarsity/zen_article.js
+++ yvarsity/zen_article.js
@@ -1,24 +1,33 @@
 let el = document.querySelector(".main"),
-head = document.querySelector('head'),
+head = document.querySelector("head"),
 arr = [];
 
-fetch(new Request( `https://yclients.zendesk.com/api/v2/help_center/ru/articles/${el.id}.json` ))
+fetch(
+new Request(
+  `https://yclients.zendesk.com/api/v2/help_center/ru/articles/${el.id}.json`
+)
+)
 .then(responce => responce.json())
 .then(data => (arr = data.article.body))
-.then(() => showArticle())
-
-
+.then(() => showArticle());
 
 function showArticle() {
-el.insertAdjacentHTML('beforeend', `<div class="article-body markdown">${arr}</div>`);
-head.insertAdjacentHTML('beforeend',`<link rel="stylesheet" href="https://p18.zdassets.com/hc/theming_assets/708440/358822/style.css">`);
-head.insertAdjacentHTML('beforeend', `
+addEl(el, `<div class="article-body markdown">${arr}</div>`);
+addEl(
+  head,
+  `<link rel="stylesheet" href="https://p18.zdassets.com/hc/theming_assets/708440/358822/style.css">`
+);
+addEl(
+  head,
+  `
     <style>
-        .main {
-            max-width: 680px;
-            margin:auto;
-        }
-    </style>`);
+    .main {
+      max-width: 680px;
+      margin:auto;
+    }
+    </style>`
+);
 }
-
-function addEl(el, html) el.el.insertAdjacentHTML('beforeend', html);
+function addEl(el, html) {
+el.insertAdjacentHTML("beforeend", html);
+}
