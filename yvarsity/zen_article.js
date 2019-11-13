a4c76deeab44e6c9a7ceb856598e1b9fa5562e27
mknzev <maksknyazev@gmail.com>
2019-11-13 20:22:03 +0300

    fix

--- yvarsity/zen_article.js
+++ yvarsity/zen_article.js
@@ -1,25 +1,27 @@
 let el = document.querySelector(".main"),
-head = document.querySelector("head"),
-arr = [];
+  head = document.querySelector("head"),
+  arr = [];
 
 fetch(
-new Request(
-  `https://yclients.zendesk.com/api/v2/help_center/ru/articles/${el.id}.json`
+  new Request(
+    `https://yclients.zendesk.com/api/v2/help_center/ru/articles/${el.id}.json`
+  )
 )
-)
-.then(responce => responce.json())
-.then(data => (arr = data.article.body))
-.then(() => loadContent());
-
+  .then(responce => responce.json())
+  .then(data => (arr = data.article.body))
+  .then(() => loadContent());
 
 function loadContent() {
-addEl(el, `<div class="article-body markdown">${arr}</div>`);
-addEl(head,`<link rel="stylesheet" href="https://mknzev.github.io/yvarsity/style.css">`);
-fooDisable();
+  addEl(el, `<div class="article-body markdown">${arr}</div>`);
+  addEl(
+    head,
+    `<link rel="stylesheet" href="https://mknzev.github.io/yvarsity/style.css">`
+  );
+  fooDisable();
 }
 function addEl(el, html) {
-el.insertAdjacentHTML("beforeend", html);
+  el.insertAdjacentHTML("beforeend", html);
 }
 function fooDisable() {
-    document.querySelector('#tildacopy').remove();
+  document.querySelector("#tildacopy").remove();
 }
