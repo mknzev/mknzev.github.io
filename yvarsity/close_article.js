b250d02428a35bb5aff866c6fe927fbfaeee1b6b
MK <42518971+mknzev@users.noreply.github.com>
2021-04-28 13:34:42 +0300

    Create close_article.js

---
+++ yvarsity/close_article.js
@@ -0,0 +1,46 @@
+let el = document.querySelector(".main"),
+  head = document.querySelector("head"),
+  arr = [];
+
+fetch(
+  new Request(
+    `http://ducknest.ga:8000/article/${el.id}`
+  )
+)
+  .then(responce => responce.json())
+  .then(data => (arr = data.article.body))
+  .then(() => loadContent());
+
+
+
+function loadContent() {
+  addEl(el,`<div class="article-body markdown">${arr}</div>`);
+  addEl(head,`<link rel="stylesheet" href="https://p18.zdassets.com/hc/theming_assets/708440/358822/style.css">`);
+  addEl(el,` <style>
+            h1, h2 {
+                margin-top: 30px;
+            }
+            h3, h4 {
+                margin-top: 15px;
+            }
+            h5, h6 {
+                margin-top: 10px;
+            }
+            .main {
+                max-width: 750px;
+                margin: 100px;
+                }
+            .t-records ~ div {
+                display: none;
+            }
+            .markdown img {
+                margin: 25px;
+            }
+            a {
+                color: rgba(36, 132, 218, 1)!important;
+            }
+            </style>`);
+}
+function addEl(el, html) {
+  el.insertAdjacentHTML("beforeend", html);
+}
